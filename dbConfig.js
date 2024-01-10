6d178cc5f84c126ca242e5ea7b692ed81de8a60f
floo420 <florian.brasseur@student.ehb.be>
2024-01-10 23:02:15 +0100

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,16 +1,24 @@
-const { Sequelize } = require('sequelize');
-const EventModel = require('./api/models/eventModel');
+const mysql = require('mysql2');
 
-const sequelize = new Sequelize('BackendProjectNodeJS', 'root', 'root', {
-  host: '127.0.0.1',
-  port: '8889',
-  dialect: 'mysql',
-  logging: false,
+// Create a connection to the MySQL server
+const connection = mysql.createConnection({
+  host: '127.0.0.1', 
+  user: 'root',      
+  password: 'root',  
+  database: 'BackendProjectNodeJS', 
+  port: '8889'      
 });
 
-const Event = EventModel(sequelize, Sequelize.DataTypes);
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
 
-module.exports = {
-  sequelize,
-  Event
-};
+// Perform MySQL queries here
+
+// Close the MySQL connection when done
+// connection.end();
