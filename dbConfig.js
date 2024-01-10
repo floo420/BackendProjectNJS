4012a66a196c92b7e3f3d4ce7882549cf8fbfa05
floo420 <florian.brasseur@student.ehb.be>
2024-01-10 13:56:46 +0100

    fix database

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,13 @@
-// dbConfig.js
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('BackendProjectNodeJS', 'root', 'root', {
   host: '127.0.0.1',
+  port: '8889',
   dialect: 'mysql',
   logging: false, // You can enable logging for debugging if needed
 });
 
-module.exports = sequelize;
+module.exports = {
+  sequelize, // Export the Sequelize instance
+  Event: sequelize.import('./eventModel'), // Import the Event model and export it
+};
