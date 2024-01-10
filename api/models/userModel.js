a5d7d6319db45461587f0387f7a2a99d024676e7
floo420 <florian.brasseur@student.ehb.be>
2024-01-10 15:58:37 +0100

    fix database

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,30 +2,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../dbConfig'); 
 
-const User = sequelize.define('User', {
-  first_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  last_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true, // Validate email format
-    },
-  },
-  age: {
-    type: DataTypes.INTEGER,
-    validate: {
-      min: 18, // Validate age as a minimum of 18
-    },
-  },
-  // Define other fields as needed
-});
+// models/userModel.js
 
-module.exports = User;
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // Validate email format
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 18, // Validate age as a minimum of 18
+      },
+    },
+    // Define other fields as needed
+  });
+
+  return User;
+};
