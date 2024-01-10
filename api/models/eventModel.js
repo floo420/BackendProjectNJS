f67599f377f90042f4e4b75d23f42c6335373479
floo420 <florian.brasseur@student.ehb.be>
2024-01-10 15:57:25 +0100

    fix database

--- api/models/eventModel.js
+++ api/models/eventModel.js
@@ -2,23 +2,28 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../dbConfig'); 
 
-const Event = sequelize.define('Event', {
-  event_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  event_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  event_location: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  event_description: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-});
+// models/eventModel.js
 
-module.exports = Event;
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {
+    event_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    event_location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    event_description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  return Event;
+};
+
--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
+const EventModel = require('./api/models/eventModel');
 
 const sequelize = new Sequelize('BackendProjectNodeJS', 'root', 'root', {
   host: '127.0.0.1',
   port: '8889',
   dialect: 'mysql',
-  logging: false, // You can enable logging for debugging if needed
+  logging: false,
 });
 
+const Event = EventModel(sequelize, Sequelize.DataTypes);
+
 module.exports = {
-  sequelize, // Export the Sequelize instance
-  Event: sequelize.import('./eventModel'), // Import the Event model and export it
+  sequelize,
+  Event
 };
