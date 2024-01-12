e8768f6febe1c21156443370d70957952250376f
floo420 <florian.brasseur@student.ehb.be>
2024-01-12 14:31:21 +0100

    Update userModel.js

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,8 +1,8 @@
 // user.js
 const User = {
-  createUser: async (connection, user) => {
+  createUser: async (pool, user) => {
     try {
-      const [results] = await connection.execute(
+      const [results] = await pool.query(
         'INSERT INTO users (first_name, last_name, email, phone_number, birthdate) VALUES (?, ?, ?, ?, ?)',
         [user.first_name, user.last_name, user.email, user.phone_number, user.birthdate]
       );
@@ -11,9 +11,19 @@ const User = {
       throw error;
     }
   },
-  getUserById: async (connection, userId) => {
+
+  getAllUsers: async (pool) => {
     try {
-      const [results] = await connection.execute('SELECT * FROM users WHERE user_id = ?', [userId]);
+      const [results] = await pool.query('SELECT * FROM users');
+      return results; // Return all users
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getUserById: async (pool, userId) => {
+    try {
+      const [results] = await pool.query('SELECT * FROM users WHERE user_id = ?', [userId]);
       return results[0]; // Return the user data
     } catch (error) {
       throw error;
